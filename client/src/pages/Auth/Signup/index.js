5167f86ee469deac9f15d24501ed3ff431a4c266
Esra Cebi <esracebi.81@gmail.com>
2021-08-28 17:55:10 +0300

    orjinal hali validation

--- client/src/pages/Auth/Signup/index.js
+++ client/src/pages/Auth/Signup/index.js
@@ -1,22 +1,10 @@
 import React from 'react';
 import { Flex, Box, Heading, FormControl, FormLabel, Input, Button } from "@chakra-ui/react";
 import { useFormik } from 'formik';
-import * as Yup from "yup";
+import validationSchema from './validations'; 
 
 
 function Signup() {
-    const validate = Yup.object({
-        name: Yup.string()
-          .min(3, "Must be 3 characters or more")
-          .required("Required"),
-        email: Yup.string().email("Email is invalid").required("Email is required"),
-        password: Yup.string()
-          .min(6, "Password must be at least 6 characters")
-          .required("Password is required"),
-        confirmPassword: Yup.string()
-          .oneOf([Yup.ref("password"), null], "Password must match")
-          .required("Confirm password is required"),
-      });
 
     const formik =useFormik({
         initialValues:{
@@ -24,7 +12,7 @@ function Signup() {
             password: "",
             passwordCorfirm: "",
         },
-        validationSchema:{validate},
+        validationSchema,
         onSubmit: async (values, bag)=> {
             console.log(values);
         },
--- client/src/pages/Auth/Signup/validations.js
+++ client/src/pages/Auth/Signup/validations.js
@@ -1,17 +1,19 @@
 
-import * as Yup from "yup";
+import * as yup from "yup";
 
-const validate = Yup.object({
-    name: Yup.string()
-      .min(3, "Must be 3 characters or more")
-      .required("Required"),
-    email: Yup.string().email("Email is invalid").required("Email is required"),
-    password: Yup.string()
-      .min(6, "Password must be at least 6 characters")
-      .required("Password is required"),
-    confirmPassword: Yup.string()
-      .oneOf([Yup.ref("password"), null], "Password must match")
-      .required("Confirm password is required"),
-  });
+const validations = yup.object().shape({
+	email: yup
+		.string()
+		.email("Gerçerli bir email girin.")
+		.required("Zorunlu alan."),
+	password: yup
+		.string()
+		.min(3, "Parolanız en az 3 karakter olmalıdır")
+		.required(),
+	passwordConfirm: yup
+        .string()
+        .oneOf([yup.ref("password")],"Parolalar uyuşmuyor.")
+        .required(),
+});
 
-export default validate;
+export default validations;
